9f1c99acb6068833016baf87aeea35cf100b61d3
Steph4n <108176326+Stepheeeen@users.noreply.github.com>
2024-01-14 14:41:38 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumberGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        int numberOfAttempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Guess the Number Game!");
+        System.out.println("I've selected a number between " + lowerBound + " and " + upperBound + ". Can you guess it?");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfAttempts++;
+
+            if (userGuess < lowerBound || userGuess > upperBound) {
+                System.out.println("Please enter a valid guess between " + lowerBound + " and " + upperBound + ".");
+            } else if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number " + randomNumber + " correctly in " +
+                                   numberOfAttempts + " attempts.");
+            }
+        }
+
+        scanner.close();
     }
 }
